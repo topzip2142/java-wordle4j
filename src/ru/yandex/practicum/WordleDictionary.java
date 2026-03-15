a72fa076e74d77d215084b90c414a3e97aade1f1
topzip <topzip2142@yandex.ru>
2026-03-15 18:37:30 +0300

    Реализовать основные классы словаря, загрузки слов в словарь

--- src/ru/yandex/practicum/WordleDictionary.java
+++ src/ru/yandex/practicum/WordleDictionary.java
@@ -1,14 +1,75 @@
 package ru.yandex.practicum;
 
+import ru.yandex.practicum.exceptions.WordNotFoundInDictionaryException;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-/*
-этот класс содержит в себе список слов List<String>
-    его методы похожи на методы списка, но учитывают особенности игры
-    также этот класс может содержать рутинные функции по сравнению слов, букв и т.д.
- */
 public class WordleDictionary {
-
     private List<String> words;
+    private Random random = new Random();
+    private final PrintWriter log;
 
+    public WordleDictionary(List<String> words, PrintWriter log) {
+        this.words = new ArrayList<>(words);
+        this.log = log;
+    }
+
+    public String getRandomWord() {
+        String word = words.get(random.nextInt(words.size()));
+        log.println("Выбрано слово: " + word);
+        return word;
+    }
+
+    public static String normalizeWord(String word) {
+        return word.toLowerCase().replace('ё', 'е').trim();
+    }
+
+    public boolean containsWord(String word) throws WordNotFoundInDictionaryException {
+        if (word == null) {
+            return false;
+        }
+        log.println("Проверка наличия слова '" + word + "' в словаре: " + words.contains(normalizeWord(word)));
+        return words.contains(normalizeWord(word));
+    }
+
+    public static String compareWords(String guess, String answer) {
+        if (guess == null || guess.length() != 5) {
+            throw new IllegalArgumentException("Слово может состоять только из 5 букв ");
+        }
+        char[] result = new char[5];
+        boolean[] answerUsed = new boolean[5];
+        for (int i = 0; i < 5; i++) {
+            if (guess.charAt(i) == answer.charAt(i)) {
+                result[i] = '+';
+                answerUsed[i] = true;
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            if (result[i] == '+') {
+                continue;
+            }
+            char guessChar = guess.charAt(i);
+            boolean found = false;
+
+            for (int j = 0; j < 5; j++) {
+                if (!answerUsed[j] && answer.charAt(j) == guessChar) {
+                    result[i] = '^';
+                    answerUsed[j] = true;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                result[i] = '-';
+            }
+        }
+        return new String(result);
+    }
+
+    public List<String> getWords() {
+        return words;
+    }
 }
